be2a44da56dbde66c96209868e5086ef4d2b3c47
Hiratake <hiratake@hiratake.xyz>
2021-08-04 18:42:30 +0900

    ci: stylelintを追加

---
+++ stylelint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recess-order',
+  ],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      { ignoreAtRules: ['use', 'forward', 'mixin', 'include'] },
+    ],
+  },
+}
