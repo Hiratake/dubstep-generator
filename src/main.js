3e18040c793ceff4c4a0e0511320fab4c1f9206e
Hiratake <hiratake@hiratake.xyz>
2021-08-06 20:09:45 +0900

    feat: 選択した任意の画像をcanvasに描画できるよう変更
    #2

--- src/main.js
+++ src/main.js
@@ -1,14 +1,95 @@
 const canvas = document.getElementById('preview')
-const width = canvas.getAttribute('width')
-const height = canvas.getAttribute('height')
+const upload = document.getElementById('upload')
+const file = document.getElementById('file')
 
-console.log(width)
-console.log(height)
-
-if (canvas.getContext) {
-  const ctx = canvas.getContext('2d')
-  ctx.fillStyle = 'rgb(200, 0, 0)'
-  ctx.fillRect(10, 10, 50, 50)
-  ctx.fillStyle = 'rgba(0, 0, 200, 0.5)'
-  ctx.fillRect(30, 30, 50, 50)
+const createImage = async (image) => {
+  return new Promise((resolve, reject) => {
+    const node = document.createElement('img')
+    const reader = new FileReader()
+    node.file = image
+    reader.onload = ((image) => {
+      return (e) => {
+        image.src = e.target.result
+        resolve(image)
+      }
+    })(node)
+    reader.onerror = (e) => {
+      reject(e)
+    }
+    reader.readAsDataURL(image)
+  })
 }
+
+const checkImage = (image) => {
+  if (typeof image !== 'undefined') {
+    const jpeg = image.type && image.type === 'image/jpeg'
+    const png = image.type && image.type === 'image/png'
+    return !!(jpeg || png)
+  }
+  else {
+    return false
+  }
+}
+
+const showImage = (name, image) => {
+  image.classList.add('c-upload__image')
+  upload.prepend(image)
+}
+
+const uploadImage = (image) => {
+  const name = image.name
+  createImage(image).then((res) => {
+    showImage(name, res.cloneNode())
+    draw(res)
+  })
+}
+
+const draw = (image) => {
+  if (canvas.getContext) {
+    const ctx = canvas.getContext('2d')
+    const canvasWidth = canvas.getAttribute('width')
+    const canvasHeight = canvas.getAttribute('height')
+    const imageWidth = image.width
+    const imageHeight = image.height
+    const renderWidth = image.width >= image.height
+      ? 200
+      : imageWidth * (200 / imageHeight)
+    const renderHeight = image.width >= image.height
+      ? imageHeight * (200 / imageWidth)
+      : 200
+    ctx.drawImage(
+      image,
+      (canvasWidth - renderWidth) / 2,
+      (canvasHeight - renderHeight) / 2,
+      renderWidth,
+      renderHeight,
+    )
+  }
+}
+
+upload.addEventListener('dragover', (e) => {
+  e.preventDefault()
+  upload.classList.add('is-dragover')
+})
+upload.addEventListener('dragleave', (e) => {
+  e.preventDefault()
+  upload.classList.remove('is-dragover')
+})
+
+upload.addEventListener('drop', (e) => {
+  const image = e.dataTransfer.files
+  e.preventDefault()
+  upload.classList.remove('is-dragover')
+  if (checkImage(image[0])) {
+    file.files = image
+    uploadImage(file.files[0])
+  }
+})
+file.addEventListener('change', (e) => {
+  if (checkImage(e.target.files[0])) {
+    uploadImage(file.files[0])
+  }
+  else {
+    file.value = ''
+  }
+}, false)
