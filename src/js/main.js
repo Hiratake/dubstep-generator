82820cee191d3b4a2469ce14223a923557742f90
Hiratake <hiratake@hiratake.xyz>
2021-08-15 13:41:12 +0900

    feat: アニメーションの速度を買えられるよう変更
    Fixes #10

--- src/js/main.js
+++ src/js/main.js
@@ -47,21 +47,40 @@ let animation
 })()
 
 /**
+ * 描画するアニメーションのフレーム数を計算する関数
+ * @param {Number} base デフォルトのフレーム数
+ * @param {Number} speed アニメーションの速さ
+ * @returns {Number} フレーム数
+ */
+const getFrameCount = (base, speed) => {
+  const arr = [2, 1.8, 1.6, 1.4, 1.2, 1, 0.9, 0.8, 0.7, 0.6, 0.5]
+  return Math.floor(base * arr[speed])
+}
+
+/**
  * エフェクトを適用する関数
  * @param {Object} context Canvasのコンテキスト
  * @param {Object} image 描画する画像
  * @param {Object} effect 適用するエフェクト
  * @param {Number} currentFrame 現在のフレーム番号
+ * @param {Number} speed アニメーションの速さ
  * @returns {Number} 次のフレーム番号
  */
-const draw = (context, image, effect, currentFrame = 1) => {
+const drawFrame = (
+  context,
+  image,
+  effect,
+  currentFrame,
+  speed = options.speed,
+) => {
   const width = image.width >= image.height
     ? context.canvas.width
     : image.width * (context.canvas.height / image.height)
   const height = image.width >= image.height
     ? image.height * (context.canvas.width / image.width)
     : context.canvas.height
-  const rate = currentFrame / effect.framecount
+  const framecount = getFrameCount(effect.framecount, speed)
+  const rate = currentFrame / framecount
 
   context.save()
   context.clearRect(0, 0, context.canvas.width, context.canvas.height)
@@ -74,7 +93,7 @@ const draw = (context, image, effect, currentFrame = 1) => {
     height,
   )
   context.restore()
-  return currentFrame >= effect.framecount ? 1 : currentFrame + 1
+  return currentFrame >= framecount ? 1 : currentFrame + 1
 }
 
 file({ targetId: fileInputId, dropAreaId: fileDropAreaId }, async (file) => {
@@ -99,7 +118,7 @@ file({ targetId: fileInputId, dropAreaId: fileDropAreaId }, async (file) => {
   clearInterval(animation)
   animation = setInterval(() => {
     try {
-      currentFrame = draw(
+      currentFrame = drawFrame(
         preview.context,
         uploadImage.element,
         effect,
