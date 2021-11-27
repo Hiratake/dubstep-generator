f8e4d014e4b697ba8c28a6f78460c9c8120dce43
Hiratake <hiratake@hiratake.xyz>
2021-11-27 16:29:27 +0900

    build: stylelintを14系に更新

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,12 +1,15 @@
 module.exports = {
   extends: [
-    'stylelint-config-standard',
+    'stylelint-config-standard-scss',
     'stylelint-config-recess-order',
   ],
+  customSyntax: 'postcss-html',
   rules: {
     'at-rule-no-unknown': [
       true,
       { ignoreAtRules: ['use', 'forward', 'mixin', 'include', 'extend'] },
     ],
+    'selector-class-pattern': '^[a-z][a-z0-9-_]+[a-z0-9]$',
+    'scss/percent-placeholder-pattern': '^[a-z][a-z0-9-_]+[a-z0-9]$',
   },
 }
